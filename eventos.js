7a9f83930af29ac753bb153b002a096d59661804
sebasfabr <sebastianfarbicio@gmail.com>
2023-08-10 14:10:46 -0300

    Cambio final

---
+++ eventos.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let divElement = document.getElementById('divSaludo');
+
+    divElement.addEventListener('click', () => {
+        alert('Hola! Soy el div');
+    });
+});
+
+
+
+
+
