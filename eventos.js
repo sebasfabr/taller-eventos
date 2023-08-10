2afd0726a502cce60adee953497b81a0a68d1cac
sebasfabr <sebastianfarbicio@gmail.com>
2023-08-10 14:46:59 -0300

    Solucion al problema del clic

--- eventos.js
+++ eventos.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     let divElement = document.getElementById('divSaludo');
 
-    divElement.addEventListener('click', () => {
-        alert('Hola! Soy el div');
+    divElement.addEventListener('click', (event) => {
+        if (!event.target.matches('button')) { 
+            alert('Hola! Soy el div');
+        }
     });
 });
-
-
-
-
 
